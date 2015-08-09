f1621e218ac3b38953bea4c7c3b881d79f3631ab
Reed Mangino <reed@themanginos.com>
2015-08-09 16:57:32 -0400

    Create a new topic/bookmark from incoming email messages.

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -12,43 +12,30 @@ class IncomingController < ApplicationController
     body_plain = params["stripped-text"]
     bookmark_url = URI.extract(body_plain).first
 
-    puts "from : #{from_address}"
-    puts "body : #{body_plain}"
-    puts "url  : #{bookmark_url}"
-    puts "user : #{user}"
-
     if user && bookmark_url
       # The email's subject represents a topic. If no subject is present
       # add the bookmark to the "default" topic.
       topic_string = params[:subject]
-      if nil == topic_string
+      if nil == topic_string || topic_string.blank?
         topic = Topic.default_topic_for_user(user)
       else
-# is this an existing or new topic?
         topic = Topic.find_by(title: topic_string)
         if nil == topic
           topic = Topic.new(title: topic_string, user: user)
         end
       end
 
+      puts "from  : #{from_address}"
+      puts "url   : #{bookmark_url}"
+      puts "topic : #{topic.title}"
+      puts "user  : #{user.name}"
+      puts "body  : #{body_plain}"
+
       bookmark = Bookmark.create!(url: bookmark_url, topic: topic)
       topic.bookmarks << bookmark
       topic.save!
       puts " --- SUCCESS ---"
     end
-
-
-
-    # You put the message-splitting and business magic here.
-    # Find the user by using params[:sender]
-    # Find the topic by using params[:subject]
-    # Assign the url to a variable after retreiving it from params["body-plain"]
-
-    # Check if user is nil, if so, create and save a new user
-
-    # Check if the topic is nil, if so, create and save a new topic
-
-    # Now that you're sure you have a valid user and topic, build and save a new bookmark
 
     # Assuming all went well.
     head 200
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,16 +5,16 @@ class Topic < ActiveRecord::Base
   # Returns the default topic for user. If no default topic exists for user
   # a new default topic will be created.
   def self.default_topic_for_user(user)
-    default_topic = Topic.find_by(title: default_topic_name, user: user)
+    default_topic = Topic.find_by(title: self.default_topic_name, user: user)
     if nil == default_topic
-      default_topic = Topic.create!(title: default_topic_name, user: user)
+      default_topic = Topic.create!(title: self.default_topic_name, user: user)
     end
     default_topic
   end
 
 private
 
-  def default_topic_name
+  def self.default_topic_name
     "default"
   end
 end
