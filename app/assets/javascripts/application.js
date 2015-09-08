d8839f3371a1451c8c6954e9dbaeee8c9ee647f0
Reed Mangino <reed@themanginos.com>
2015-09-08 09:38:07 -0400

    Add the javascript so that we can send click events to the blocmetrics api.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,8 @@
 //= require bootstrap
 //= require_tree .
 
+// This is for the blocmetrics app. Every time we click on a page we send that event to the
+// api portion of the blocmetrics app.
 $( document ).ready(function() {
   $( window ).click( function() {
       console.log( "You clicked!!!!!!" );
@@ -29,7 +31,8 @@ blocmetrics.report = function(eventName) {
   var event = { name: eventName };
   var request = new XMLHttpRequest();
 
-  request.open("POST", "https://reeds-blocmetrics.herokuapp.com/api/events", true);
+  //request.open("POST", "https://reeds-blocmetrics.herokuapp.com/api/events", true);
+  request.open("POST", "https://localhost/api/events", true);
   request.setRequestHeader('Content-Type', 'application/json');
 
   request.send(JSON.stringify(event));
